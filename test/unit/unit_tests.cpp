d1807efa84cd8b5378e172fdb8de04c1c6ecc3e0
LucasOSRodrigues <lucasosrodrigues13@gmail.com>
2026-06-01 19:45:45 -0300

    test: adicionar unit_tests

---
+++ test/unit/unit_tests.cpp
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include "unit_tests.h"
+
+/**
+ * @file unit_tests.cpp
+ * @brief Unit tests for global functions.
+ *
+ * This file contains implementations of unit tests for global functions
+ * (functions not belonging to any class).
+ *
+ * Currently, there are no global functions in the Vensim project,
+ * so this file contains only the runner function stub.
+ */
+
+/**
+ * @brief Runs all unit tests for global functions.
+ */
+void run_unit_test_globals(void) {
+    // No global functions to test yet
+}
---
+++ test/unit/unit_tests.h
@@ -0,0 +1,15 @@
+#ifndef UNIT_TESTS
+#define UNIT_TESTS
+
+/**
+ * @file unit_tests.h
+ * @brief Unit tests for global functions.
+ *
+ * This file contains declarations for unit tests of global functions
+ * (functions not belonging to any class).
+ */
+
+// Runner function
+void run_unit_test_globals(void);
+
+#endif // UNIT_TESTS
